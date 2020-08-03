bd45c24b47d4457115c460571628802296a37aca
Axurj <acerj2468@gmail.com>
2020-08-03 16:45:51 -0700

    Added more to HTML + JS

--- chart.js
+++ chart.js
@@ -1,6 +1,21 @@
 // Variables recieved from the form
-let nameRecieved = document.getElementById('nameTag');
-let numRecieved = document.getElementById('numTag');
+const nameRecieved = document.getElementById('nameTag');
+const numRecieved = document.getElementById('numTag');
+const button = document.getElementById('submitTag');
 
 // Array
-let containArray = [];
+let nameArray = [];
+let numArray = [];
+
+// Eventlistener + Functions
+button.addEventListener('click', function (e){
+    e.preventDefault();
+    nameArray.push(nameRecieved.value);
+    numArray.push(numRecieved.value);
+    nameRecieved.value = '';
+    numRecieved.value = 0;
+    if(nameArray.length > 5 && numArray.length > 5){
+        document.getElementById('container').classList.add('show');
+        document.getElementById('container').classList.remove('hidden');
+    }
+}
