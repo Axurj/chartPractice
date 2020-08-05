a568396f112b2c91381242d4b9e5aed0f90b4562
Axurj <acerj2468@gmail.com>
2020-08-05 15:26:29 -0700

    Commented chart related items out and added arrays + local storage

--- chart.js
+++ chart.js
@@ -8,14 +8,20 @@ let nameArray = [];
 let numArray = [];
 
 // Eventlistener + Functions
-button.addEventListener('click', function (e){
-    e.preventDefault();
+button.addEventListener('click', function (event){
+    event.preventDefault();
+    // nameRecieved.value = '';
+    // numRecieved.value = 0;
     nameArray.push(nameRecieved.value);
     numArray.push(numRecieved.value);
-    nameRecieved.value = '';
-    numRecieved.value = 0;
-    if(nameArray.length > 5 && numArray.length > 5){
-        document.getElementById('container').classList.add('show');
-        document.getElementById('container').classList.remove('hidden');
+    console.log('Names: ', nameArray);
+    console.log('Points: ', numArray);
+    // if(nameArray.length > 5 && numArray.length > 5){
+    //     document.getElementById('container').classList.add('show');
+    //     document.getElementById('container').classList.remove('hidden');
+    // }
+    if(numArray.length >= 5 && nameArray.length >= 5){
+        localStorage.setItem('Name: ', nameArray);
+        localStorage.setItem('Points: ', numArray);
     }
-}
+})
